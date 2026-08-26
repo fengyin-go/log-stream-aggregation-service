ffd72674ae772d391371d1b1c683e15c3afd0773
fengyin-go <1621476774@qq.com>
2026-08-26 21:44:03 +0800

    bug: 48-log-aggregation【30】-025

---
+++ internal/service/runtime_contract.go
@@ -0,0 +1,7 @@
+package service
+
+import "log-aggregation/internal/store"
+
+type RetentionCursorLeaseCoordinator struct { pool *store.RetentionCursorLeasePool }
+func NewRetentionCursorLeaseCoordinator(p *store.RetentionCursorLeasePool) *RetentionCursorLeaseCoordinator { return &RetentionCursorLeaseCoordinator{pool: p} }
+func (c *RetentionCursorLeaseCoordinator) Process(items []string) (processed int, err error) { for range items { lease, err := c.pool.Acquire(); if err != nil { return processed, err }; defer lease.Close(); processed++ }; return processed, nil }
