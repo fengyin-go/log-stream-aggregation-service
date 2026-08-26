41d2acf1e6dbf210ff218142c7fac56d05b19e07
fengyin-go <1621476774@qq.com>
2026-08-26 21:43:44 +0800

    red-test: 48-log-aggregation【30】-015

---
+++ internal/service/runtime_contract.go
@@ -0,0 +1,7 @@
+package service
+
+import "log-aggregation/internal/store"
+
+type IndexReaderLeaseCoordinator struct { pool *store.IndexReaderLeasePool }
+func NewIndexReaderLeaseCoordinator(p *store.IndexReaderLeasePool) *IndexReaderLeaseCoordinator { return &IndexReaderLeaseCoordinator{pool: p} }
+func (c *IndexReaderLeaseCoordinator) Process(items []string) (processed int, err error) { for range items { lease, err := c.pool.Acquire(); if err != nil { return processed, err }; defer lease.Close(); processed++ }; return processed, nil }
