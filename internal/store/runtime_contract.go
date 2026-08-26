506fab423c863cb85561963a95c55f4bcb4ee46f
fengyin-go <1621476774@qq.com>
2026-08-26 21:44:54 +0800

    bug: 48-log-aggregation【30】-027

---
+++ internal/store/runtime_contract.go
@@ -0,0 +1,9 @@
+package store
+
+import "sync"
+
+type VersionedState struct { Version int; State string }
+type NotificationReadVersionStore struct { mu sync.Mutex; states map[string]VersionedState }
+func NewNotificationReadVersionStore() *NotificationReadVersionStore { return &NotificationReadVersionStore{states: make(map[string]VersionedState)} }
+func (s *NotificationReadVersionStore) Update(key string, version int, state string) bool { s.mu.Lock(); defer s.mu.Unlock(); s.states[key] = VersionedState{Version: version, State: state}; return true }
+func (s *NotificationReadVersionStore) Get(key string) VersionedState { s.mu.Lock(); defer s.mu.Unlock(); return s.states[key] }
